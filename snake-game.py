ca010e120f2f13fc85a187ef7a6829e61ec44123
Jerry Padilla <jerrypadilla23@gmail.com>
2018-07-30 06:50:25 -0500

    Update snake-game.py

--- snake-game.py
+++ snake-game.py
@@ -26,10 +26,10 @@ win.nodelay(1)                                                                  
 key = KEY_RIGHT                                                                     # Assigning value for key
 score = 0                                                                           # Assigning value to score
 count = 0
-snake = [[15,15],[0,0]]                                                             # snake[0] is the starting position. Each new list in the arry is a "body part" so in this case the snake starts with 2 body parts.
+snake = [[15,15],[0,0]]                                                             # snake[0] is the starting position. Each new list within the list is a "body part" so in this case the snake starts with 2 body parts.
 
 food = [randint(1, 49), randint(1, 119)]                                            # coordinates of where the first piece of food will be in the window. Random makes it more fun than the same spot each time.
-win.addch(food[0], food[1], '$')                                                    # Assigns a character for that particular spot on the window where the food will start. Both index 0, and index 1 of Food will need the same char symbol. Think about it.
+win.addch(food[0], food[1], '$')                                                    # Assigns a character for that particular spot on the window where the food will start. Both index 0, and index 1, of Food will need the same char symbol. Think about it.
 
 
 while key != 27:                                                                    # This is where the fun happens. 27 ASCII code for 'ESC'. When you hit it, while it's moving. It will break the while loop and end the window.
@@ -47,7 +47,7 @@ while key != 27:                                                                
     event = win.getch()                                                             # var event waiting for win.getch() to capture character from user so it can exist.
     key = key if event == -1 else event                                             
 
-    if key == ord(' '):                                                             # empty space == '32' which is ASCII for 'space bar'. If the keystroke is spacebar, key is assigned to 32 and gets stuck in this if statement. Then key is reassigned to -1. Thus placing it in a loop of being reassigned to -1 in the code above.
+    if key == ord(' '):                                                             # empty space == '32' which is ASCII for 'space bar'. If the keystroke is spacebar, key is assigned to 32 and gets stuck in this if statement. Then key is reassigned to -1.
         key = -1                                                                    # one (Pause/Resume)
 
         while key != ord(' '):                                                      # Tried to explain the loop but it takes too much text. Figure it out :D
@@ -60,7 +60,7 @@ while key != 27:                                                                
     if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:                      # If an invalid key is pressed it reverts back to the stored prevKey
         key = prevKey
 
-    # This insert actually adds a new 'head' to the snake, then the if statement below about the food, the else: in that statement actually pops the last array within the array so it appears to stay the same length when in fact it does increase here.
+    # This insert actually adds a new list (head) to the snake, then the if statement below about the food, the else: in that statement actually pops the last list within the list so it appears to stay the same length when in fact it does increase here.
     snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
 
     # Will exit the game if the borders are touched by the snake.
@@ -69,21 +69,21 @@ while key != 27:                                                                
         break
 
 
-    if snake[0] in snake[1:]:                                       # If the snakehead's [x,y] values match another value within the snakes array. It will exit.  
+    if snake[0] in snake[1:]:                                       # If the snakehead's [x,y] list values match another value within the snakes list. It will exit.  
         break
 
     
     if snake[0] == food:                                            # When snakes postion on the window matches the foods position = "eaten". This if statement will execute.
-        food = []                                                   # Reassigns food to an empty array, ready to be reassigned.    
+        food = []                                                   # Reassigns food to an empty list, ready to be reassigned.    
         score += 1                                                  # LVL UP BITCH!
         while food == []:                                           # Executed because the window is hungry for food.
-            food = [randint(1, 48), randint(1, 118)]                # Random [x,y]'s generated for the food, placing it somewhere on the window next.
-            if food in snake:                                       # If the new generated foods values match a set of values within the snakes array, it will reset the while. 
+            food = [randint(1, 48), randint(1, 118)]                # Random [x,y] list generated for the food, placing it somewhere on the window next.
+            if food in snake:                                       # If the new generated food list values match a set of values within the snakes list, it will reset the while. 
                 food = []                                          
         win.addch(food[0], food[1], '$')                            # WHen the new food passes the while validator, it will then be assigned to actual food variable.
     else:    
-        last = snake.pop()                                          # Since the snake.insert statement above is adding new snake heads, if the snake doesn't eat the food, then it's here that the snake.pop() pops the last [x,y] in the snake array, but the tail end of the simulated snake.
-        win.addch(last[0], last[1], ' ')                            # Now the snakes array doesn't contain this [x,y] but it's still printed to the screen. So this is called to fill that particular [x,y] coordinate with blanks. Essentially eraseing the symbol.
+        last = snake.pop()                                          # Since the snake.insert statement above is adding new snake list (head), if the snake doesn't eat the food, then it's here that the snake.pop() pops the last [x,y] list within the snake list. (the tail end of the simulated snake)
+        win.addch(last[0], last[1], ' ')                            # Now the snakes list doesn't contain this [x,y] but it's still printed to the screen. So this is called to fill that particular [x,y] coordinate with blanks. Essentially eraseing the symbol.
         
     win.addch(snake[0][0], snake[0][1], '@')                        # This keeps the head a '@'
     win.addch(snake[1][0], snake[1][1], '$')                        # Keeps the body of the snake '$' making it look more like a snake in action!
